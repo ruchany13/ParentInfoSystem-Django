32ad08fd6809f088659467c4bf24daa25df57a18
Ruchan Yalçın <ruchany13@gmail.com>
2025-10-26 20:51:05 +0300

    added announcement apps

--- parentinfo/announcement/admin.py
+++ parentinfo/announcement/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import GeneralNotice, InstitutionNotice
+from core.admin import InstitutionScopedAdminMixin
 
-# Register your models here.
+
+@admin.register(GeneralNotice)
+class GeneralNoticeAdmin(admin.ModelAdmin):
+    list_display = ("name", "time", "notice")
+    search_fields = ("name", "notice")
+    list_filter = ("time",)
+
+
+@admin.register(InstitutionNotice)
+class InstitutionNoticeAdmin(InstitutionScopedAdminMixin, admin.ModelAdmin):
+    list_display = ("name", "instutionnotice")
+    search_fields = ("name", "instutionnotice")
--- parentinfo/announcement/models.py
+++ parentinfo/announcement/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from core.models import Institution
+from datetime import date
 
-# Create your models here.
+class GeneralNotice(models.Model):
+    name = models.CharField(max_length=200, verbose_name='İsim')
+    notice = models.TextField(verbose_name='Announcement')
+    image = models.ImageField(upload_to='announcement/', blank=True, null=True, verbose_name='Resim')
+    time = models.DateField(default=date.today, verbose_name='Date')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "General Announcement"
+        verbose_name_plural = "Genaral Announcements"
+        ordering = ["-time"]
+        get_latest_by = "time" 
+
+class InstitutionNotice(models.Model):
+    name = models.ForeignKey(GeneralNotice, on_delete=models.CASCADE, related_name="institution_notice", verbose_name="Program")
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE, related_name="institution_notice", verbose_name="Institution")
+    instutionnotice = models.TextField(verbose_name='Program schedule, additional announcement information, announcements:')
+
+    def get_message_as_html(self):
+        return self.instutionnotice.replace('\n', '<br>')
+
+    class Meta:
+        verbose_name = "Institution Announcement"
+        verbose_name_plural = "Institution Announcements"
+        ordering = ["-name"]
